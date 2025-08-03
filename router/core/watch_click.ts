1b085bd9fdf3504dd0b727f66615f6055cc9ecde
git <client@funxdata.com>
2025-08-03 13:25:42 +0800

    update same path and params no redirect

--- router/core/watch_click.ts
+++ router/core/watch_click.ts
@@ -1,15 +1,28 @@
 import { PagesRouterInfo } from "@/types/router.ts";
 
-export const WatchClickLink= (router: PagesRouterInfo)=> {
+export const WatchClickLink = (router: PagesRouterInfo) => {
   document.addEventListener("click", (event: MouseEvent) => {
     const target = (event.target as HTMLElement).closest("a");
+
     if (
       target instanceof HTMLAnchorElement &&
       target.href &&
       target.origin === location.origin
     ) {
       event.preventDefault();
-      router.navigate(target.pathname);
+
+      const targetUrl = new URL(target.href);
+      const currentUrl = new URL(location.href);
+
+      // 路径和参数都一致，不跳转
+      if (
+        targetUrl.pathname === currentUrl.pathname &&
+        targetUrl.search === currentUrl.search
+      ) {
+        return;
+      }
+
+      router.navigate(target.pathname + target.search);
     }
   });
-}
+};
