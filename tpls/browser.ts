9cb98c7a6dd8d045277d3a8fadfe88502ec109b6
git <client@funxdata.com>
2025-08-03 12:35:13 +0800

    fix template browser package

--- tpls/browser.ts
+++ tpls/browser.ts
@@ -1,9 +1,6 @@
 import { Tpl as TplCore } from "./core.ts";
-
 export class Tpl extends TplCore {}
-
 // deno-lint-ignore no-explicit-any
-declare const globalThis:any;
-(() => {
-    globalThis.TplToHtml = new Tpl({cache:false}); 
-})();
+declare const globalThis:any
+const TplToHtml = new Tpl({cache:false});
+globalThis.TplToHtml = TplToHtml;
