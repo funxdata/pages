24dffb1c6dbb49793d0f86194a955a1e3619aa16
git <client@funxdata.com>
2025-08-14 08:24:25 +0800

    update url

--- router/core/check_url.ts
+++ router/core/check_url.ts
@@ -12,9 +12,9 @@ export const check_url=  (target:HTMLAnchorElement):boolean=>{
 
 export const is_only_Pagination=(to:string):boolean=>{
   const from_url = new URL(globalThis.location.href);
-  const to_url = new URL(to);
-  if(from_url.pathname == to_url.pathname&&to_url.search!=undefined&&to_url.search!=null&&to_url.search!=""){
-    return true;
-  }
-  return false;
+  const to_url = new URL(to, globalThis.location.href);
+  return (
+    from_url.pathname === to_url.pathname &&
+    Boolean(to_url.search)
+  );
 };
