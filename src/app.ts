fc043a0eba0772a89968fdb93d218fdf133941db
git <client@funxdata.com>
2025-08-26 16:49:49 +0800

    fix replace(/\n/g)

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,14 @@ const TplToHtml = (globalThis as any)["TplToHtml"] as Tpl;
 
 const App = document.getElementById("app") as HTMLElement;
 // 1. 编译并注册模板
-TplToHtml.loadTemplate("header", TplToHtml.compile!("<header><div><%= it.title %></div></header>"));
+const header_html = `
+<header>
+
+<div><%= it.title %></div>
+
+</header>
+`
+TplToHtml.loadTemplate("header", TplToHtml.compile!(header_html));
 TplToHtml.loadTemplate("footer", TplToHtml.compile!("<footer>© <%= it.year %></footer>"));
 
 // console.log(TplToHtml.templates)
@@ -12,7 +19,7 @@ TplToHtml.loadTemplate("footer", TplToHtml.compile!("<footer>© <%= it.year %></
 // console.log(aa);
 // // 2. 渲染模板
 const result = await TplToHtml.renderStringAsync(
-  "<%~ include('header', it) %><p><%= it.hello %></p><%~ include('footer', it) %>",
+  "<%~ include('header', it) %><p><%= it.hello %></p><%~ include('footer', it) %><% console.log('hello world') %> ",
   { title: "My Site", year: 2025,hello:"hello world" }
 );
 console.log(result)
